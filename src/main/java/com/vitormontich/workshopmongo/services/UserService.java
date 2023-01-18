c27220558824d9c68204ad770bb454be393ce05e
vmontich <vmontich@gmail.com>
2023-01-17 21:16:31 -0300

    Obtendo um usuário por ID

--- src/main/java/com/vitormontich/workshopmongo/services/UserService.java
+++ src/main/java/com/vitormontich/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.vitormontich.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vitormontich.workshopmongo.domain.User;
 import com.vitormontich.workshopmongo.repositories.UserRepository;
+import com.vitormontich.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,10 @@ public class UserService {
 	public List<User> findAll() {
 		return userRepository.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 
 }
