d8ea26c9ccdeb0be4e0875d284b4cdd43f0f66da
vmontich <vmontich@gmail.com>
2023-01-17 21:27:55 -0300

    Inserção de usuário com POST[C

--- src/main/java/com/vitormontich/workshopmongo/services/UserService.java
+++ src/main/java/com/vitormontich/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vitormontich.workshopmongo.domain.User;
+import com.vitormontich.workshopmongo.dto.UserDTO;
 import com.vitormontich.workshopmongo.repositories.UserRepository;
 import com.vitormontich.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-		}
+	}
+	
+	public User insert(User user) {
+		return userRepository.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
+	}
 
 }
