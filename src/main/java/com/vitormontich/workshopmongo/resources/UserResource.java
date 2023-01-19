a7d0836fb41c6ad467f6b9965b994ffb33b385e2
vmontich <vmontich@gmail.com>
2023-01-19 20:47:13 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/vitormontich/workshopmongo/resources/UserResource.java
+++ src/main/java/com/vitormontich/workshopmongo/resources/UserResource.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.vitormontich.workshopmongo.domain.Post;
+import com.vitormontich.workshopmongo.domain.Post;
 import com.vitormontich.workshopmongo.domain.User;
 import com.vitormontich.workshopmongo.dto.UserDTO;
 import com.vitormontich.workshopmongo.services.UserService;
@@ -61,6 +63,12 @@ public class UserResource {
 		user.setId(id);
 		user = userService.update(user);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts( @PathVariable String id ) {
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
 	}
 
 }
