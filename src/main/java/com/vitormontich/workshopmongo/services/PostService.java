6e9dce7220cac20febbbf07c37d334574f5a01fd
vmontich <vmontich@gmail.com>
2023-01-19 20:52:36 -0300

    Objtendo um post por id

---
+++ src/main/java/com/vitormontich/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.vitormontich.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vitormontich.workshopmongo.domain.Post;
+import com.vitormontich.workshopmongo.repositories.PostRepository;
+import com.vitormontich.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
