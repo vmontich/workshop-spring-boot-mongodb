3e3db495b2772f4b9fd99cb81d99c12b906ddb90
vmontich <vmontich@gmail.com>
2023-01-20 09:20:38 -0300

    Consulta simples com @Query

--- src/main/java/com/vitormontich/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/vitormontich/workshopmongo/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.vitormontich.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.vitormontich.workshopmongo.domain.Post;
@@ -11,5 +12,8 @@ import com.vitormontich.workshopmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String> {
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
 
 }
